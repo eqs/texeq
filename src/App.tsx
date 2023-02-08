a923f05846b2b18cbecbfff4894fdf1ec0ea1f66
eqs <murashige.satoshi.mi1@gmail.com>
2023-02-09 02:56:58 +0900

    Rendering MathJax

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,42 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
-import { BlockMath } from 'react-katex';
-import 'katex/dist/katex.min.css';
+import { mathjax } from 'mathjax-full/js/mathjax'
+import { TeX } from "mathjax-full/js/input/tex";
+import { SVG } from "mathjax-full/js/output/svg";
+import { STATE } from "mathjax-full/js/core/MathItem";
+import { liteAdaptor } from 'mathjax-full/js/adaptors/liteAdaptor'
+import { RegisterHTMLHandler } from 'mathjax-full/js/handlers/html'
+
+// https://github.com/mathjax/MathJax/issues/2385#issuecomment-1253051223
+
+const adaptor = liteAdaptor()
+RegisterHTMLHandler(adaptor)
+
+const tex = new TeX({ packages: ["base", "ams"] });
+const svg = new SVG({ fontCache: "none" });
+const tex_html = mathjax.document("", {
+  InputJax: tex,
+  OutputJax: svg,
+});
+
+const MathComponent = (props: { text: string }) => {
+  const [state, setState] = useState<string>("");
+  useEffect(() => {
+    let result = tex_html.convert(props.text);
+    setState(adaptor.innerHTML(result));
+  }, [props]);
+  return (
+      <div>
+        <img src={`data:image/svg+xml;utf8,${state}`} />
+      </div>
+  ); // `
+}
 
 
 function App() {
   return (
     <div className="App">
-      <BlockMath math='\mathcal F[f(x)] = \int_{-\infty}^\infty f(x)e^{-i\omega x} \mathrm dx'/>
+      <MathComponent text='F(\omega) = \mathcal F[f(x)] = \int_{-\infty}^\infty f(x)e^{-i\omega x} \mathrm dx'/>
     </div>
   );
 }
